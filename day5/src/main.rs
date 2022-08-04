1cf0fdd679c8b61253cc0128ed972635ab503763
Markus Spath <markus@hackr.de>
2022-08-04 08:42:31 +0200

    Day 5 pt2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     let input = include_str!("input.txt");
     let result_breakfast = breakfast(input);
     println!("{}", result_breakfast);
+    let result_lunch = lunch(input);
+    println!("{}", result_lunch);
 }
 
 fn breakfast(input: &str) -> usize {
@@ -23,20 +25,14 @@ fn breakfast(input: &str) -> usize {
             .split(',')
             .map(|n| n.parse().unwrap())
             .collect();
-        //let c = vec![left[0], left[1], right[0], right[1]];
         let c = [left[0], left[1], right[0], right[1]];
         c
     }
     ).collect();
 
-    let horizontal_vertical: Vec<[i32; 4]> = coordinates.into_iter()
-        .filter(|c|c[0] == c[2] || c[1] == c[3])
-        .collect();
-    println!("{:?}", horizontal_vertical.len());
-
     let mut points: HashMap<(i32, i32), i32> = HashMap::new();
 
-    for [x1, y1, x2, y2] in horizontal_vertical {
+    for [x1, y1, x2, y2] in coordinates {
         if x1 == x2 {
             for y in y1.min(y2)..=y1.max(y2) {
                 *points.entry((x1, y)).or_default() += 1;
@@ -49,4 +45,68 @@ fn breakfast(input: &str) -> usize {
     }
 
     points.values().filter(|n| **n > 1).count()
+}
+
+fn lunch(input: &str) -> usize {
+    let coordinates: Vec<[i32; 4]> = input
+    .trim()
+    .split('\n')
+    .into_iter()
+    .map(|line| {
+        let mut parts = line.split(" -> ");
+        let left: Vec<i32> = parts.next()
+            .unwrap()
+            .split(',')
+            .map(|n| n.parse().unwrap())
+            .collect();
+        let right: Vec<i32> = parts.next()
+            .unwrap()
+            .split(',')
+            .map(|n| n.parse().unwrap())
+            .collect();
+        let c = [left[0], left[1], right[0], right[1]];
+        c
+    }
+    ).collect();
+
+    let mut points: HashMap<(i32, i32), i32> = HashMap::new();
+
+    for [x1, y1, x2, y2] in coordinates {
+        if x1 == x2 {
+            for y in y1.min(y2)..=y1.max(y2) {
+                *points.entry((x1, y)).or_default() += 1;
+            }
+        } else if y1 == y2 {
+            for x in x1.min(x2)..=x1.max(x2) {
+                *points.entry((x, y1)).or_default() += 1;
+            }
+        } else {
+            // we assume the input is clean
+            let dx = if x2 - x1 > 0 { 1 } else { -1 };
+            let dy = if y2 - y1 > 0 { 1 } else { -1 };
+            let mut x = x1;
+            let mut y = y1;
+            *points.entry((x, y)).or_default() += 1;
+            while x != x2 {
+                x += dx;
+                y += dy;
+                *points.entry((x, y)).or_default() += 1;
+            }
+        }
+    }
+
+    points.values().filter(|n| **n > 1).count()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn check_breakfast() {
+        assert_eq!(super::breakfast(include_str!("input.txt")), 6005);
+    }
+
+    #[test]
+    fn check_lunch() {
+        assert_eq!(super::lunch(include_str!("input.txt")), 23864);
+    }
 }
